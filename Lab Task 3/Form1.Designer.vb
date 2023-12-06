96ac740e705de71831918848142f92b9b0dab949
Nur Mahirah <nurmahirah911@gmail.com>
2023-12-06 20:43:02 +0800

    second commit

--- Lab Task 3/Form1.Designer.vb
+++ Lab Task 3/Form1.Designer.vb
@@ -23,120 +23,123 @@ Partial Class FormApps
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormApps))
-        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
-        Me.ViewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.AddToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.UpdateToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
-        Me.DeleteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.TextBox = New System.Windows.Forms.TextBox()
         Me.ListApps = New System.Windows.Forms.ListBox()
+        Me.btnView = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
-        '
-        'MenuStrip1
-        '
-        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
-        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ViewToolStripMenuItem, Me.AddToolStripMenuItem, Me.UpdateToolStripMenuItem1, Me.DeleteToolStripMenuItem, Me.ExitToolStripMenuItem})
-        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
-        Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(700, 28)
-        Me.MenuStrip1.TabIndex = 0
-        Me.MenuStrip1.Text = "MenuStrip1"
-        '
-        'ViewToolStripMenuItem
-        '
-        Me.ViewToolStripMenuItem.ForeColor = System.Drawing.Color.Black
-        Me.ViewToolStripMenuItem.Name = "ViewToolStripMenuItem"
-        Me.ViewToolStripMenuItem.Size = New System.Drawing.Size(53, 24)
-        Me.ViewToolStripMenuItem.Text = "View"
-        '
-        'AddToolStripMenuItem
-        '
-        Me.AddToolStripMenuItem.ForeColor = System.Drawing.Color.Blue
-        Me.AddToolStripMenuItem.Name = "AddToolStripMenuItem"
-        Me.AddToolStripMenuItem.Size = New System.Drawing.Size(49, 24)
-        Me.AddToolStripMenuItem.Text = "Add"
-        '
-        'UpdateToolStripMenuItem1
-        '
-        Me.UpdateToolStripMenuItem1.ForeColor = System.Drawing.Color.Orange
-        Me.UpdateToolStripMenuItem1.Name = "UpdateToolStripMenuItem1"
-        Me.UpdateToolStripMenuItem1.Size = New System.Drawing.Size(70, 24)
-        Me.UpdateToolStripMenuItem1.Text = "Update"
-        '
-        'DeleteToolStripMenuItem
-        '
-        Me.DeleteToolStripMenuItem.ForeColor = System.Drawing.Color.Red
-        Me.DeleteToolStripMenuItem.Name = "DeleteToolStripMenuItem"
-        Me.DeleteToolStripMenuItem.Size = New System.Drawing.Size(65, 24)
-        Me.DeleteToolStripMenuItem.Text = "Delete"
-        '
-        'ExitToolStripMenuItem
-        '
-        Me.ExitToolStripMenuItem.ForeColor = System.Drawing.Color.DarkRed
-        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
-        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(45, 24)
-        Me.ExitToolStripMenuItem.Text = "Exit"
-        '
-        'TextBox
-        '
-        Me.TextBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox.Location = New System.Drawing.Point(240, 61)
-        Me.TextBox.Name = "TextBox"
-        Me.TextBox.Size = New System.Drawing.Size(244, 28)
-        Me.TextBox.TabIndex = 1
         '
         'ListApps
         '
-        Me.ListApps.BackColor = System.Drawing.Color.Azure
-        Me.ListApps.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListApps.BackColor = System.Drawing.Color.LavenderBlush
+        Me.ListApps.Font = New System.Drawing.Font("Microsoft New Tai Lue", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.ListApps.FormattingEnabled = True
-        Me.ListApps.ItemHeight = 22
-        Me.ListApps.Location = New System.Drawing.Point(41, 121)
+        Me.ListApps.ItemHeight = 24
+        Me.ListApps.Location = New System.Drawing.Point(44, 94)
         Me.ListApps.Name = "ListApps"
-        Me.ListApps.Size = New System.Drawing.Size(618, 422)
+        Me.ListApps.Size = New System.Drawing.Size(419, 316)
         Me.ListApps.TabIndex = 3
+        '
+        'btnView
+        '
+        Me.btnView.BackColor = System.Drawing.Color.White
+        Me.btnView.Font = New System.Drawing.Font("Malgun Gothic", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnView.Location = New System.Drawing.Point(507, 94)
+        Me.btnView.Name = "btnView"
+        Me.btnView.Size = New System.Drawing.Size(106, 46)
+        Me.btnView.TabIndex = 5
+        Me.btnView.Text = "View"
+        Me.btnView.UseVisualStyleBackColor = False
+        '
+        'btnAdd
+        '
+        Me.btnAdd.BackColor = System.Drawing.Color.White
+        Me.btnAdd.Font = New System.Drawing.Font("Malgun Gothic", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.ForeColor = System.Drawing.Color.Blue
+        Me.btnAdd.Location = New System.Drawing.Point(507, 160)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(106, 46)
+        Me.btnAdd.TabIndex = 6
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = False
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.BackColor = System.Drawing.Color.White
+        Me.btnUpdate.Font = New System.Drawing.Font("Malgun Gothic", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnUpdate.ForeColor = System.Drawing.Color.Orange
+        Me.btnUpdate.Location = New System.Drawing.Point(507, 226)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(106, 46)
+        Me.btnUpdate.TabIndex = 7
+        Me.btnUpdate.Text = "Update"
+        Me.btnUpdate.UseVisualStyleBackColor = False
+        '
+        'btnDelete
+        '
+        Me.btnDelete.BackColor = System.Drawing.Color.White
+        Me.btnDelete.Font = New System.Drawing.Font("Malgun Gothic", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDelete.ForeColor = System.Drawing.Color.Red
+        Me.btnDelete.Location = New System.Drawing.Point(507, 298)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(106, 46)
+        Me.btnDelete.TabIndex = 8
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.White
+        Me.btnExit.Font = New System.Drawing.Font("Malgun Gothic", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(507, 364)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(106, 46)
+        Me.btnExit.TabIndex = 9
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(144, 66)
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Segoe Print", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(35, 21)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(68, 20)
-        Me.Label1.TabIndex = 4
-        Me.Label1.Text = "Search"
+        Me.Label1.Size = New System.Drawing.Size(302, 54)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "List of Application"
         '
         'FormApps
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.LightSteelBlue
-        Me.ClientSize = New System.Drawing.Size(700, 595)
-        Me.Controls.Add(Me.Label1)
+        Me.BackgroundImage = Global.Lab_Task_3.My.Resources.Resources.bg
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(675, 505)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.btnView)
         Me.Controls.Add(Me.ListApps)
-        Me.Controls.Add(Me.TextBox)
-        Me.Controls.Add(Me.MenuStrip1)
+        Me.Controls.Add(Me.Label1)
+        Me.DoubleBuffered = True
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "FormApps"
         Me.Text = "App Management System"
-        Me.MenuStrip1.ResumeLayout(False)
-        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents MenuStrip1 As MenuStrip
-    Friend WithEvents ViewToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents AddToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents UpdateToolStripMenuItem1 As ToolStripMenuItem
-    Friend WithEvents DeleteToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents TextBox As TextBox
     Friend WithEvents ListApps As ListBox
+    Friend WithEvents btnView As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnExit As Button
     Friend WithEvents Label1 As Label
 End Class
--- Lab Task 3/Form1.vb
+++ Lab Task 3/Form1.vb
@@ -1,109 +1,110 @@
-﻿Imports System.Text
-
-Public Class FormApps
-    Private apps As New List(Of String)()
-    Private MaxAppsCount As Integer = 10
+﻿Public Class FormApps
+    ' One-dimensional array to store app names
+    Private appsArray As Integer = 7
+    Private appsCount As Integer = 0
+    Private apps(appsArray - 1) As String
 
     Private Sub clearList()
         ListApps.Items.Clear() 'clear the list of Apps
     End Sub
 
-    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
-        'exit the application
-        Application.Exit()
-    End Sub
-
-    Private Sub AddToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AddToolStripMenuItem.Click
-        ' Get the app name from the TextBox
-        Dim appName As String = TextBox.Text.Trim()
-
-        ' Check if the app name is not empty
-        If Not String.IsNullOrEmpty(appName) Then
-            If apps.Count >= MaxAppsCount Then
-                ' Handle the situation where the array is full
-                MessageBox.Show("Cannot add the app. The maximum number of apps has been reached.", "Apps Management System", MessageBoxButtons.OK, MessageBoxIcon.Error)
-            Else
-                ' Add the app to the array or collection
-                apps.Add(appName)
-
-                ' Clear the TextBox
-                TextBox.Text = ""
-
-                ' Show a message box indicating that the app has been added
-                MessageBox.Show($"{appName} has been added.", "Apps Management System", MessageBoxButtons.OK, MessageBoxIcon.Information)
-            End If
-        End If
-    End Sub
-
-    Private Sub ViewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ViewToolStripMenuItem.Click
-        ' Clear the ListBox
+    Private Sub DisplayListApps()
         ListApps.Items.Clear()
 
-        ' Loop through the apps list and add each app to the ListBox
-        For Each app As String In apps
-            ListApps.Items.Add(app)
+        For i As Integer = 0 To appsCount - 1
+            ListApps.Items.Add(apps(i))
         Next
     End Sub
 
-    Private Sub DeleteToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DeleteToolStripMenuItem.Click
-        ' Get the app name to be deleted from the TextBox
-        Dim appNameToDelete As String = TextBox.Text.Trim()
+    Private Sub btnView_Click(sender As Object, e As EventArgs) Handles btnView.Click
+        'for user to view the list of all installed apps
+        DisplayListApps()
+    End Sub
 
-        ' Check if the app name is not empty
-        If Not String.IsNullOrEmpty(appNameToDelete) Then
+    Private Sub addApp(appName As String)
+        'check if there is space in the array
+        If appsCount < appsArray Then
+            'add the app to the array
+            apps(appsCount) = appName
+            appsCount += 1
+            MessageBox.Show($"{appName} has been added.", "Add Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            MessageBox.Show("Array is full! Cannot add more apps.", "Add Information", MessageBoxButtons.OK, MessageBoxIcon.Stop)
+        End If
+    End Sub
 
-            ' Check if the app exists in the array or collection
-            If apps.Contains(appNameToDelete) Then
-                ' Remove the app from the array or collection
-                apps.Remove(appNameToDelete)
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim appName As String = InputBox("Enter name of the app: ")
 
-                ' Remove the app from the ListBox
-                ListApps.Items.Remove(appNameToDelete)
+        If Not String.IsNullOrEmpty(appName) Then
+            'add the app into the array
+            addApp(appName)
 
-                ' Clear the TextBox
-                TextBox.Text = ""
+            'display the updated list of apps
+            DisplayListApps()
+            clearList()
+        End If
+    End Sub
 
-                ' Show a message box indicating that the app has been deleted
-                MessageBox.Show($"{appNameToDelete} has been deleted.", "Apps Management System", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    Private Sub updateApp(index As Integer, newName As String)
+        'update the app with new name
+        apps(index) = newName
+        MessageBox.Show($"The app is successfully updated! New name: {newName}", "Update Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        ' allow user to select an apps to update 
+        Dim selectedApp As Integer = ListApps.SelectedIndex
+
+        If selectedApp <> -1 Then
+            'get new name for apps
+            Dim newAppName As String
+            newAppName = InputBox("Enter the New name for this apps: ")
+            MessageBox.Show($"The app is successfully update! New apps name: {newAppName}", "Update Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+            'check if user enter a new name
+            If Not String.IsNullOrEmpty(newAppName) Then
+                updateApp(selectedApp, newAppName)
+                DisplayListApps()
             Else
-                ' Show a message box indicating that the app does not exist
-                MessageBox.Show("The app does not exist.", "Apps Management System", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                MessageBox.Show("Please select an app to update.", "Update Information", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
             End If
         End If
     End Sub
 
-    Private Sub UpdateToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles UpdateToolStripMenuItem1.Click
-        ' Get the selected app's name from the ListBox
-        If ListApps.SelectedIndex >= 0 Then
-            Dim selectedAppName As String = ListApps.SelectedItem.ToString()
+    Private Sub DeleteApp(index As Integer)
+        'remove the app
+        Dim removeAppName As String = apps(index)
+        Dim confirmessage = MessageBox.Show($"Are you sure to delete {removeAppName}?", "Delete Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
 
-            ' Get the new app name from the TextBox
-            Dim updatedAppName As String = TextBox.Text.Trim()
+        If confirmessage = DialogResult.Yes Then
+            For i As Integer = index To appsCount - 2
+                apps(i) = apps(i + 1)
+            Next
 
-            ' Check if the new app name is not empty
-            If Not String.IsNullOrEmpty(updatedAppName) Then
-                ' Update the app's name in the array or collection
-                Dim index As Integer = apps.IndexOf(selectedAppName)
-                If index >= 0 Then
-                    apps(index) = updatedAppName
-
-                    ' Clear the TextBox
-                    TextBox.Text = ""
-
-                    ' Show a message box indicating that the app has been updated
-                    MessageBox.Show($"{updatedAppName} has been updated.", "Apps Management System", MessageBoxButtons.OK, MessageBoxIcon.Information)
-                End If
-            End If
+            'decrement the app count
+            appsCount -= 1
+            MessageBox.Show($"{removeAppName} app has been deleted.", "Delete Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
         End If
     End Sub
 
-    Private Sub ListApps_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListApps.SelectedIndexChanged
-        ' Get the selected app's name from the ListBox
-        If ListApps.SelectedIndex >= 0 Then
-            Dim selectedAppName As String = ListApps.SelectedItem.ToString()
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        'allow the user to select an app to remove
+        Dim selectedApp As Integer = ListApps.SelectedIndex
 
-            ' Display the app's name in a TextBox for editing
-            TextBox.Text = selectedAppName
+        If selectedApp <> -1 Then
+            'remove the selected app
+            DeleteApp(selectedApp)
+
+            'display the updated list of apps
+            DisplayListApps()
+        End If
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Dim result As DialogResult = MessageBox.Show("Are you sure you want to exit the application?", "Exit Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If result = DialogResult.Yes Then
+            Application.Exit()
         End If
     End Sub
 End Class
